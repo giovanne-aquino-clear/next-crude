988de6f6b582e545575174cbfa53f57a574d1760
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-19 11:14:58 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home(){
+  return(
+    <div className= {`
+      flex h-screen justify-center items-center
+      bg-gradient-to-r from-purple-500 to-blue-600
+      text white
+   
+   `}>
+      <span>Texto</span>
+    </div>
+  )
+}
